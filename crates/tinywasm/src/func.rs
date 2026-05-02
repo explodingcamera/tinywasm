d3c6b705a3b1035adda66a92131e9ddf59fb46ab
Henry <mail@henrygressmann.de>
2026-05-02 15:47:49 +0200

    chore: implement tuple traits for up to 16 arguments
    Signed-off-by: Henry <mail@henrygressmann.de>

--- crates/tinywasm/src/func.rs
+++ crates/tinywasm/src/func.rs
@@ -554,6 +554,10 @@ macro_rules! impl_tuple {
         $macro!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10);
         $macro!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11);
         $macro!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12);
+        $macro!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13);
+        $macro!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14);
+        $macro!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15);
+        $macro!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16);
     };
 }
 
